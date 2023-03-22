f7288dc4a05b05542d40f65f74d873ad3f3e240c
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-03-22 16:01:09 +0100

    AppTitle component style

--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -19,7 +19,17 @@ export default {
 <!-- CSS -->
 <style scoped>
 h1 {
-    color: blue;
+    /* Text gradient */
+    background: -webkit-linear-gradient(rgba(156, 252, 248, 1) 11.2%, rgba(110, 123, 251, 1) 91.1%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+
+    /* Properties */
+    font-family: "Lato";
+    font-size: 4.5rem;
+    font-style: italic;
+    margin-top: 3.125rem;
+    text-align: center;
 }
 </style>
 <!-- // CSS -->
