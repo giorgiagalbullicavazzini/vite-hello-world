c3c83695ddc35fd451edd7368e52686a09571543
giorgiagalbullicavazzini <giorgia.galbullicavazzini@gmail.com>
2023-03-22 15:34:13 +0100

    AppTitle component creation

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
+<!-- Import components -->
 <script>
+import AppTitle from './components/AppTitle.vue';
 
+export default {
+    components: {
+        AppTitle,
+    }
+}
 </script>
+<!-- // Import components -->
 
 <template>
-
+    <AppTitle></AppTitle>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,25 @@
+<!-- Title data -->
+<script>
+export default {
+    data() {
+        return {
+            title: 'La mia prima app con Vite!'
+        }
+    }
+}
+</script>
+<!-- // Title data -->
+
+<!-- HTML -->
+<template>
+    <h1>{{ title }}</h1>
+</template>
+<!-- // HTML -->
+
+<!-- CSS -->
+<style scoped>
+h1 {
+    color: blue;
+}
+</style>
+<!-- // CSS -->
